aec746062579374cac05a4259122faba3db5be8b
Nithi Kerdmongkhon <Kerdmongkhon2545@gmail.com>
2025-01-14 11:23:47 +0700

    ลบออก

--- src/app/(contents)/add_department/page.js
+++ src/app/(contents)/add_department/page.js
@@ -1,85 +1,105 @@
 "use client";
-import { useState } from 'react';
+import { useState } from "react";
 import Navbar from "@/app/components/navbar";
 import Footer from "@/app/components/footer";
 import postData from "@/app/components/CLUD/post";
 import Handle_Click from "@/app/components/handle/handleclick";
-import { useRouter } from 'next/navigation';
-import styles from '@/app/styles/add_department.module.css';
+import { useRouter } from "next/navigation";
+import styles from "@/app/styles/add_department.module.css";
 
 export default function AddDepartment() {
-    const [name, setName] = useState('');
-    const [total, setTotal] = useState('');
-    const [rname, setRname] = useState('');
-    const roundOptions = ["เช้า ช่วง 1", "เช้า ช่วง 2", "บ่าย ช่วง 1", "บ่าย ช่วง 2"];
+  const [name, setName] = useState("");
+  const [total, setTotal] = useState("");
+  const [rname, setRname] = useState("");
+  const roundOptions = [
+    "เช้า ช่วง 1",
+    "เช้า ช่วง 2",
+    "บ่าย ช่วง 1",
+    "บ่าย ช่วง 2",
+  ];
 
-    const router = useRouter();
+  const router = useRouter();
 
-    const handleSubmit = async () => {
-        const departmentData = { name, total, rname };
-        console.log('Department data:', departmentData); //ทดสอบ vercel
+  const handleSubmit = async () => {
+    const departmentData = { name, total, rname };
+    console.log("Department data:", departmentData);
 
-        try {
-            const result = await postData('add_department', departmentData);
-            console.log('Department added successfully:', result);
-            setName('');
-            setTotal('');
-            setRname('');
-            router.push("/faculty");
-        } catch (error) {
-            console.error('Error adding department:', error);
-        }
-    };
+    try {
+      const result = await postData("add_department", departmentData);
+      console.log("Department added successfully:", result);
+      setName("");
+      setTotal("");
+      setRname("");
+      router.push("/faculty");
+    } catch (error) {
+      console.error("Error adding department:", error);
+    }
+  };
 
-    return (
-        <div>
-            <Navbar />
-            <div className={styles.BodyContainer}>
-                <h1>เพิ่มหน่วยงาน</h1>
-                <form onSubmit={(e) => { e.preventDefault(); handleSubmit(); }} className={styles.containerForm}>
-                    <div className={styles.nameFaculty}>
-                        <label>
-                            หน่วยงาน :
-                        </label>
-                        <input
-                            type="text"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            required
-                        />
-                    </div>
-                    <div className={styles.numberStyle}>
-                        <label>
-                            จำนวนเข้ารับ :
-                        </label>
-                        <input
-                            type="number"
-                            value={total}
-                            onChange={(e) => setTotal(e.target.value)}
-                            required
-                        />
-                    </div>
-                    <div className={styles.roundStyle}>
-                        <label>
-                            รอบ :
-                        </label>
-                        <select value={rname} onChange={(e) => setRname(e.target.value)} required>
-                            <option value="">เลือกรอบ</option>
-                            {roundOptions.map((option, i) => (
-                                <option key={i} value={option}>{option}</option>
-                            ))}
-                        </select>
-                    </div>
-                </form>
+  return (
+    <div>
+      <Navbar />
+      <div className={styles.BodyContainer}>
+        <h1>เพิ่มหน่วยงาน</h1>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+          }}
+          className={styles.containerForm}
+        >
+          <div className={styles.nameFaculty}>
+            <label>หน่วยงาน :</label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          <div className={styles.numberStyle}>
+            <label>จำนวนเข้ารับ :</label>
+            <input
+              type="number"
+              value={total}
+              onChange={(e) => setTotal(e.target.value)}
+              required
+            />
+          </div>
+          <div className={styles.roundStyle}>
+            <label>รอบ :</label>
+            <select
+              value={rname}
+              onChange={(e) => setRname(e.target.value)}
+              required
+            >
+              <option value="">เลือกรอบ</option>
+              {roundOptions.map((option, i) => (
+                <option key={i} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+        </form>
 
-                {/* แก้ไขเพิ่ม div เพื่อให้ปุ่มอยู่ในบรรทัดเดียวกัน */}
-                <div className={styles.buttonContainer}>
-                    <Handle_Click className={styles.buttonBack} path="/faculty" buttonText="ย้อนกลับ" />
-                    <button type="button" className={styles.buttonSave} onClick={handleSubmit}>บันทึก</button>
-                </div>
-
-            </div>
-            <Footer />
+        {/* แก้ไขเพิ่ม div เพื่อให้ปุ่มอยู่ในบรรทัดเดียวกัน */}
+        <div className={styles.buttonContainer}>
+          <Handle_Click
+            className={styles.buttonBack}
+            path="/faculty"
+            buttonText="ย้อนกลับ"
+          />
+          <button
+            type="button"
+            className={styles.buttonSave}
+            onClick={handleSubmit}
+          >
+            บันทึก
+          </button>
         </div>
-    );
+      </div>
+      <Footer />
+    </div>
+  );
 }
